13ecfd57afcad6751d232cc00ac505fa70cbcbe1
Johnny <johnnyruiz03.99@gmail.com>
2022-10-08 15:00:25 -0400

    all features implemented still checking the object information storage

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const body = document.querySelector('body')
 const table = document.querySelector('.table')
 const addBook = document.querySelector('#add-book-button')
 const newBookName = document.getElementById('form-book-title')
 const newBookAuthor = document.getElementById('form-book-author')
-const NewBookPages = document.getElementById('form-book-pages')
+const newBookPages = document.getElementById('form-book-pages')
 const newBookRead = document.getElementById('form-book-checkbox')
 const cancelButton = document.querySelector('#cancel-button')
 const confirmBook = document.querySelector('#confirm-button')
-const checkbox = document.querySelector('.book-checkbox')
 const shelf = document.querySelector('.shelf')
 const books = document.getElementsByClassName('book')
 const form = document.querySelector('#form-container')
 const div = document.createElement('div')
+const copy = books[0].cloneNode(true)
+let checkboxes = document.querySelectorAll('.book-checkbox')
 let deleteButton = document.querySelectorAll('.delete-button')
 let editButton = document.querySelectorAll('.edit-button')
 let bookNumber = 0
+checkboxes[0].addEventListener('click', updateReadStatus)
 deleteButton[0].addEventListener('click', deleteBook)
 editButton[0].addEventListener('click', editBookForm)
+confirmBook.addEventListener('click', addBookToLibrary)
+addBook.addEventListener('click', formPopup)
+cancelButton.addEventListener('click', formCancel)
 
 function book(name, author, pages, read) {
     this.name = name
@@ -25,22 +29,47 @@ function book(name, author, pages, read) {
     this.read = read
 }
 
+function updateLibrary(newBook) {
+  newBook.childNodes[3].textContent = newBookName.value
+  newBook.childNodes[5].textContent = newBookAuthor.value
+  newBook.childNodes[7].textContent = newBookPages.value + ' pages'
+  newBook.childNodes[9].firstElementChild.checked = newBookRead.checked
+}
+
+function updateReadStatus() {
+  
+}
+
 function addBookToLibrary() {
-  const clone = books[0].cloneNode(true)
-  bookNumber++
-  clone.id = `book${bookNumber}`
-  clone.childNodes[3].textContent = newBookName.value
-  clone.childNodes[5].textContent = newBookAuthor.value
-  clone.childNodes[7].textContent = NewBookPages.value + ' pages'
-  clone.childNodes[9].firstElementChild.checked = newBookRead.checked
-  shelf.appendChild(clone)
-  myLibrary.push(new book(newBookName.value, newBookAuthor.value, NewBookPages.value + ' pages', newBookRead.checked))
-  clearForm()
-  deleteButton = document.querySelectorAll('.delete-button')
-  editButton = document.querySelectorAll('.edit-button')
-  deleteButton.forEach(element => element.addEventListener('click', deleteBook))
-  editButton.forEach(element => element.addEventListener('click', editBookForm))
-  formCancel()
+  if (newBookName.value == '' || newBookAuthor.value == '' || newBookPages.value == '' || isNaN(newBookPages.value))
+    {
+      window.alert('PLEASE FILL OUT ALL FIELDS')
+    }
+  else if (books[0] == undefined) {
+    bookNumber++
+    copy.id = `book${bookNumber}`
+    updateLibrary(copy)
+    shelf.appendChild(copy)
+    myLibrary.push(new book(newBookName.value, newBookAuthor.value, newBookPages.value + ' pages', newBookRead.checked))
+    deleteButton = document.querySelectorAll('.delete-button')
+    editButton = document.querySelectorAll('.edit-button')
+    deleteButton.forEach(element => element.addEventListener('click', deleteBook))
+    editButton.forEach(element => element.addEventListener('click', editBookForm))
+    formCancel()
+  }
+  else {
+    const clone = books[0].cloneNode(true)
+    bookNumber++
+    clone.id = `book${bookNumber}`
+    updateLibrary(clone)
+    shelf.appendChild(clone)
+    myLibrary.push(new book(newBookName.value, newBookAuthor.value, newBookPages.value + ' pages', newBookRead.checked))
+    deleteButton = document.querySelectorAll('.delete-button')
+    editButton = document.querySelectorAll('.edit-button')
+    deleteButton.forEach(element => element.addEventListener('click', deleteBook))
+    editButton.forEach(element => element.addEventListener('click', editBookForm))
+    formCancel()
+  }
 }
 
 function deleteBook(book) {
@@ -58,17 +87,20 @@ function editBookForm(e) {
 }
 
 function editBook() {
-  currentBook.childNodes[3].textContent = newBookName.value
-  currentBook.childNodes[5].textContent = newBookAuthor.value
-  currentBook.childNodes[7].textContent = NewBookPages.value + ' pages'
-  currentBook.childNodes[9].firstElementChild.checked = newBookRead.checked
-  myLibrary[bookId].name = newBookName.value
-  myLibrary[bookId].author = newBookAuthor.value
-  myLibrary[bookId].pages = NewBookPages.value + ' pages'
-  myLibrary[bookId].read = newBookRead.checked
-  formCancel()
-  confirmBook.removeEventListener('click', editBook)
-  confirmBook.addEventListener('click', addBookToLibrary)
+  if (newBookName.value == '' || newBookAuthor.value == '' || newBookPages.value == '' || isNaN(newBookPages.value))
+    {
+      window.alert('PLEASE FILL OUT ALL FIELDS')
+    }
+  else {
+    updateLibrary(currentBook)
+    myLibrary[bookId].name = newBookName.value
+    myLibrary[bookId].author = newBookAuthor.value
+    myLibrary[bookId].pages = newBookPages.value + ' pages'
+    myLibrary[bookId].read = newBookRead.checked
+    formCancel()
+    confirmBook.removeEventListener('click', editBook)
+    confirmBook.addEventListener('click', addBookToLibrary)
+  }
 }
 
 function formPopup() {
@@ -87,11 +119,8 @@ function formCancel() {
 function clearForm() {
   newBookName.value = ''
   newBookAuthor.value = ''
-  NewBookPages.value = ''
+  newBookPages.value = ''
   newBookRead.checked = false
 }
 
-confirmBook.addEventListener('click', addBookToLibrary)
-addBook.addEventListener('click', formPopup)
-cancelButton.addEventListener('click', formCancel)
 let myLibrary = [new book(books[0].childNodes[3].textContent, books[0].childNodes[5].textContent, books[0].childNodes[7].textContent, books[0].childNodes[9].firstElementChild.checked)];
